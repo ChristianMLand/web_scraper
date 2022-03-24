491ab8a1691ccff179a23b00f8d4d2bc552c2c41
ChristianMLand <christianmland@outlook.com>
2022-03-23 18:11:23 -0700

    updated parser function

--- parser.py
+++ parser.py
@@ -2,53 +2,6 @@
 from bs4 import BeautifulSoup
 import xlsxwriter
 
-def parse_table(soup):
-    '''
-    Lots of inconsistencies in the html structure made this file much 
-    more complicated than it needed to be, if the original html was cleaned
-    up this function would be about half as complex
-    '''
-    table = soup.find_all("table")[1]
-    rows = table.find_all("tr")
-    rows_dict = {}
-    for i, row in enumerate(rows):
-        if i < 14:
-            tds = row.find_all("td")
-            row_data = []
-            line_cell = tds[0]
-            if line_cell.p:
-                line_cell = line_cell.p
-            if line_cell.strong:
-                line_cell = line_cell.strong
-            if line_cell.span:
-                line_cell = line_cell.span
-            line_cell = line_cell.decode_contents()
-            line_cell = line_cell.split('<')[0].strip()
-            lines = line_cell.split('/')
-            for line in lines:
-                rows_dict[line] = row_data
-            for j in range(1, len(tds)):
-                line_cell = tds[j]
-                td_str = line_cell.string
-                if line_cell.p:
-                    p_list = []
-                    for p in line_cell.find_all("p"):
-                        if p.strong:
-                            p = p.strong
-                        if p.span:
-                            p = p.span
-                        p_list.append(p.string)
-                    td_str = ''.join(p_list)
-                if line_cell.strong:
-                    line_cell = line_cell.strong
-                if line_cell.span:
-                    line_cell = line_cell.span
-                if not td_str:
-                    td_str = line_cell.string
-                td_str = td_str.replace('\xa0', "").replace("\n", "").strip()
-                row_data.append(td_str)
-    return rows_dict
-
 def filter_down_tags(tag):
     if tag.strong:
         tag = tag.strong
@@ -58,7 +11,7 @@ def filter_down_tags(tag):
         tag = tag.span
     return tag
 
-def parse_table2(soup):
+def parse_table(soup):
     '''
     Lots of inconsistencies in the html structure made this file much 
     more complicated than it needed to be, if the original html was cleaned
@@ -129,7 +82,7 @@ def write_to_worksheet(worksheet, formatted_table):
 def main():
     file = open(file='terminal_table.html', mode='r')
     soup = BeautifulSoup(markup=file, features='html.parser')
-    table_dict = parse_table2(soup)
+    table_dict = parse_table(soup)
     formatted_table = format_table(table_dict)
 
     workbook = xlsxwriter.Workbook("sample_output.xlsx")
